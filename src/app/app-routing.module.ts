ea23da4fabe70641e7f69d4186032663fe8b9336
amanmaharj <aman.mahaj@gmail.com>
2023-05-19 02:06:35 -0600

    Added the basic table outline to display the data, fixed the route for list of the data

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListEmployeeComponent } from './list-employee/list-employee.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:"",redirectTo:"human",pathMatch:"full"},
+  {path:"human",component:ListEmployeeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/model/humanResource.ts
@@ -0,0 +1,7 @@
+export class HumanResource{
+    id?: number;
+    first_name?: string;
+    last_name?: string;
+    dob?:Date;
+    hireDate?:Date;
+}
